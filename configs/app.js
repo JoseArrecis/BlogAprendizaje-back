879edd4ab915274ac2d6a9007c8f915102ff3de5
JoseArrecis <jarrecis-2020444@Kinal.edu.gt>
2025-05-06 18:11:18 -0600

    Se agrega la carpeta configs junto a los archivos app y mongo js

--- configs/app.js
+++ configs/app.js
@@ -1,3 +1,20 @@
 const configs = (app) => {
+    app.use(express.json())
+    app.use(express.urlencoded({extended: false}))
+}
+
+const routes = (app)=>{
     
+}
+
+export const initServer = async()=>{
+    const app = express()
+    try {
+        configs(app)
+        routes(app)
+        app.listen(process.env.PORT)
+        console.log(`Server rugging in port ${process.env.PORT}`)
+    } catch (err) {
+        console.error('Server init failed', err)
+    }
 }
