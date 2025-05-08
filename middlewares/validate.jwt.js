723156b1db0362586f1fa62a47c244b66cb02cac
JoseArrecis <jarrecis-2020444@Kinal.edu.gt>
2025-05-07 20:51:55 -0600

    Se agregan las carpetas helpers con sus respectivos archivos junto a los archivos encrypt y jwt

--- middlewares/validate.jwt.js
+++ middlewares/validate.jwt.js
@@ -1,19 +1,50 @@
 'use strict'
 
-import jwt from "jsonwebtoken"
+import jwt from 'jsonwebtoken'
+import { findUser } from '../helpers/db.validators.js'
 
-export const generateJwt = async(payload)=>{
-    try {
-        return jwt.sign(
-            payload,
-            process.env.SECRET_KEY,
+export const validateJwt = async(req, res, next)=>{
+    try{
+        let secretKey = process.env.SECRET_KEY
+
+        let {authorization} = req.headers
+
+        if(!authorization) return res.status(401).send({message: 'Unauthorized'})
+
+        let user = jwt.verify(authorization, secretKey)
+
+        const validateUser = await findUser(user.uid)
+        if(!validateUser) return res.status(404).send(
             {
-                expiresIn: '3h',
-                algorithm: 'HS256'
+                success: false,
+                message: 'User not found - Unauthorized'
             }
         )
-    } catch (err) {
+        req.user = user
+        next()
+    }catch(err){
         console.error(err)
-        return err
+        return res.status(401).send({message: 'Invalid token'})
+    }
+}
+
+export const isAdmin = async(req, res, next)=>{
+    try{
+        const { user } = req
+        if(!user || user.role !== 'ADMIN') return res.status(403).send(
+            {
+                success: false,
+                message: `You dont hace access | username ${user.username}` 
+            }
+        )
+        next()
+    }catch(err){
+        console.error(err)
+        return res.status(403).send(
+            {
+                success: false,
+                message: 'Unauthorized role'
+            }
+        )
     }
 }
--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,7 +1,12 @@
 export const register = async(req, res)=>{
     try {
-        
+        let data = req.body
+
+        let user = new UserActivation(data)
+        user.password = await 
+
     } catch (err) {
-        
+        console.error(err)
+        return res.status(500).send({w})
     }
 }
