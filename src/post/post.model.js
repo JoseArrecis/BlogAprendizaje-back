439cd9f56221a41717faf0d8d7a4a536283e8749
JoseArrecis <jarrecis-2020444@Kinal.edu.gt>
2025-05-10 13:14:23 -0600

    Se agrega el post.model.js

---
+++ src/post/post.model.js
@@ -0,0 +1,30 @@
+import { model, Schema } from "mongoose";
+
+const postSchema = Schema(
+    {
+        title: {
+            type: String,
+            required: [true, 'Title is required'],
+            maxLength: [75, 'Can´t be overcome 50 characters']
+        },
+        description: {
+            type: String,
+            required: [true, 'Description is required'],
+        },
+        courses: {
+            type: String,
+            required: [true, 'Courses is required'],
+            enum: ['Cocina', 'Mueblería', 'Programación']
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        status: {
+            type: Boolean,
+            default: true
+        }
+    }
+)
+
+export default model('Post', postSchema)
